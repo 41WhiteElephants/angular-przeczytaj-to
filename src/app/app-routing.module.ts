3175ddb85757b93ea7a845c8aeba3eaaf145acb1
Jakub Hurej <j.hurej@merixstudio.com>
2021-08-15 22:37:20 +0200

    remove redundant stuff for simple happy path

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from "@angular/core";
 import {RouterModule, Routes } from "@angular/router";
 import { LoginComponent } from "./auth/login/login.component";
-import { SignupComponent } from "./auth/signup/signup.component";
+// import { SignupComponent } from "./auth/signup/signup.component";
 import { GenerateAudioComponent } from "./generate-audio/generate-audio.component";
 import { LandingComponent } from "./landing/landing.component";
 
@@ -9,7 +9,8 @@ const routes :Routes = [
 {path:'', component: LandingComponent},
 {path: 'generuj', component :GenerateAudioComponent},
 {path: 'login', component :LoginComponent},
-{path: 'rejestruj', component :SignupComponent}
+// todo: add registration
+// {path: 'rejestruj', component :SignupComponent}
 ];
 
 @NgModule({
